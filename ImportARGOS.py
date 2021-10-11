abe22170c0828805b3acfe010e42329e3a32bc1a
Enikoe Bihari <enikoebihari@gmail.com>
2021-10-11 09:00:24 -0400

    Create ImportARGOS.py
    Add boiler plate code

---
+++ ImportARGOS.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 08:50:48 2021
+
+@author: eb351
+"""
+
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2020
+## Author: John.Fay@duke.edu (for ENV859)
+##---------------------------------------------------------------------
